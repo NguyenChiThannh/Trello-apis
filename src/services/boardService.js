8ffbaa4b7ad53a80b5c915623aff0072b981bff1
THANH\21110 <thanh.161003@gmail.com>
2024-01-19 03:17:56 +0700

    Finish setup CORS

--- src/services/boardService.js
+++ src/services/boardService.js
@@ -2,6 +2,7 @@ import { slugify } from '~/utils/formatters'
 import { boardModel } from '~/models/boardModel'
 import ApiError from '~/utils/ApiError'
 import { StatusCodes } from 'http-status-codes'
+import { cloneDeep } from 'lodash'
 const createNew = async (reqBody) => {
   try {
     // Xử lý logic
@@ -30,8 +31,17 @@ const getDetails = async (boardId) => {
     if (!board) {
       throw new ApiError(StatusCodes.NOT_FOUND, 'board not found')}
 
+    // CloneDeep sẽ không ảnh hưởng tới board ban đầu
+    const resBoard = cloneDeep(board)
+    // Đưa card về đúng column của nó
+    resBoard.columns.forEach(column => {
+      column.cards = resBoard.cards.filter(card => card.columnId.toString() === column._id.toString())
+    })
+
+    delete resBoard.cards
+
     // Luôn phải có return
-    return board
+    return resBoard
   } catch (error) {
     throw new Error(error)
   }
---
+++ src/utils/constants.js
@@ -0,0 +1,9 @@
+// Những domain được phép truy cập đến tài nguyên của Server
+export const WHITELIST_DOMAINS = [
+  'http://localhost:5173'
+]
+
+export const BOARD_TYPES = {
+  PUBLIC: 'public',
+  PRIVATE: 'private',
+}
